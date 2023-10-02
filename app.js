9e5b4b504e28587eb7258530c2370e27ce9a46ef
ismailattari <136378019+ismailattari@users.noreply.github.com>
2023-10-03 00:03:28 +0500

    js-assignment2

---
+++ app.js
@@ -0,0 +1,26 @@
+var myName="Muhammad ismail attari";
+alert(myName);
+
+var message="Hello World";
+// message="Muhammad Ismail Attari"
+alert(message)
+
+var student="Muhammad ismail attari"
+alert(student)
+
+var age="46 years old"
+alert(age)
+
+var expart="Certified Mobil Application development" 
+alert(expart)
+var pizza="pizza\n pizz\n piz\n pi\n p"
+alert(pizza)
+
+var myMail="my Email is ismailmadani@gmail.com"
+alert (myMail)
+
+var trying="I am trying to learn from the Book a smarter\n way to learn javaScript"
+alert(trying)
+
+var dicen="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(dicen)
